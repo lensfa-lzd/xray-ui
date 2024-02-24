601f21a39f8a49abac34027ae355efa2afc1a43c
liang <mail@lensfa.com>
2024-02-24 23:22:47 +0800

    添加conda脚本

--- conda-arm64.sh
+++ conda-arm64.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh
+bash Miniconda3-latest-Linux-aarch64.sh -b
 ./miniconda3/bin/conda init
+
