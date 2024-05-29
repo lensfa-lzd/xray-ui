d36a6654a132f0130b2a55fd391d569363659291
lensfa <mail@lensfa.com>
2024-05-29 08:39:32 +0800

    2024-05-29-备份

---
+++ systemctl-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+bash <(curl -fsSL https://gh.aws-shop.uk/https://github.com/lensfa-lzd/xray-ui/raw/main/docker-ins-al2023.sh)
+docker pull public.ecr.aws/lensfa/workos-kasm:ub22
+mkdir -p /home/ec2-user/work/xray
+mkdir -p /home/ec2-user/kasm
+curl -o /home/ec2-user/work/data.zip https://gh.aws-shop.uk/https://github.com/lensfa-lzd/xray-ui/raw/main/upload-data-2024-05-13.zip
+curl -o /home/ec2-user/kasm/docker-compose.yml https://gh.aws-shop.uk/https://github.com/lensfa-lzd/xray-ui/raw/main/docker-compose.yml
+cd /home/ec2-user/work
+unzip data.zip
+rm data.zip
