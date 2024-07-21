8261bc9f15abe8d445d26794e79c13368caa77ea
lensfa-lzd <mail@lensfa.com>
2024-07-22 01:08:36 +0800

    更新安装脚本

--- systemctl-install.sh
+++ systemctl-install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 bash <(curl -fsSL https://gh.aws-shop.uk/https://github.com/lensfa-lzd/xray-ui/raw/main/docker-ins-al2023.sh)
-docker pull public.ecr.aws/lensfa/workos-kasm:ub22
+docker pull public.ecr.aws/lensfa/workos-kasm:workos
 mkdir -p /home/ec2-user/work/xray
 mkdir -p /home/ec2-user/kasm
 curl -o /home/ec2-user/work/data.zip https://raw.githubusercontent.com/lensfa-lzd/xray-ui/main/upload-data-2024-05-29.zip
 curl -o /home/ec2-user/kasm/docker-compose.yml https://raw.githubusercontent.com/lensfa-lzd/xray-ui/main/docker-compose.yml
+# shellcheck disable=SC2164
 cd /home/ec2-user/work
 unzip data.zip
 rm data.zip
+# shellcheck disable=SC2164
 cd /home/ec2-user/kasm
 docker compose up -d
