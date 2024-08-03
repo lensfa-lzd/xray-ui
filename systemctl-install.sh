0b21d07ff480e15b8ec9136be43f6efcde806879
lensfa <mail@lensfa.com>
2024-08-04 00:29:56 +0800

    更新镜像

--- systemctl-install.sh
+++ systemctl-install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 bash <(curl -fsSL https://gh.aws-shop.uk/https://github.com/lensfa-lzd/xray-ui/raw/main/docker-ins-al2023.sh)
-docker pull public.ecr.aws/lensfa/workos-kasm:workos
+docker pull public.ecr.aws/lensfa/workos-kasm:workos-controller
 mkdir -p /home/ec2-user/work/xray
-mkdir -p /home/ec2-user/kasm
-curl -o /home/ec2-user/work/data.zip https://raw.githubusercontent.com/lensfa-lzd/xray-ui/main/upload-data-2024-05-29.zip
-curl -o /home/ec2-user/kasm/docker-compose.yml https://raw.githubusercontent.com/lensfa-lzd/xray-ui/main/docker-compose.yml
+mkdir -p /home/ec2-user/docker
+curl -o /home/ec2-user/work/data.zip https://gh.aws-shop.uk/https://raw.githubusercontent.com/lensfa-lzd/xray-ui/main/upload-data-2024-05-29.zip
+curl -o /home/ec2-user/docker/docker-compose.yml https://gh.aws-shop.uk/https://raw.githubusercontent.com/lensfa-lzd/xray-ui/main/docker-compose.yml
 # shellcheck disable=SC2164
 cd /home/ec2-user/work
 unzip data.zip
 rm data.zip
 # shellcheck disable=SC2164
-cd /home/ec2-user/kasm
+cd /home/ec2-user/docker
 docker compose up -d
